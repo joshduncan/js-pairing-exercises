a4fbee056fd0133c93dcd226839eebcd2a14c033
Paul McConnell <pmc.consult@gmail.com>
2018-08-18 16:49:31 -0500

    add solved functions that pass tests

--- captainHelpers.js
+++ captainHelpers.js
@@ -1,13 +1,24 @@
-export const firstNames = data => data;
+export const firstNames = data => data.map(c => c.first);
 
-export const firstNamesSorted = data => data;
+export const firstNamesSorted = data => data.map(c => c.first).sort();
 
-export const firstNamesSortedOnlyJs = data => data;
+export const firstNamesSortedOnlyJs = data =>
+  data
+    .map(c => c.first)
+    .sort()
+    .filter(n => n.startsWith('J'));
 
-export const totalAge = data => data;
+export const totalAge = data => data.reduce((total, c) => total + c.age, 0);
 
-export const fullNamesByAge = data => data;
+const ageSort = (a, b) => a.age - b.age;
+export const fullNamesByAge = data => data.sort(ageSort).map(c => `${c.first} ${c.last}`);
 
-export const addShip = (shipName, captains) => undefined;
+export const addShip = (shipName, captain) => {
+  captain.ship = shipName;
+  return captain;
+};
 
-export const addShipImmutable = (shipName, captain) => undefined;
+export const addShipImmutable = (shipName, captain) => ({
+  ...captain,
+  ship: shipName
+});
